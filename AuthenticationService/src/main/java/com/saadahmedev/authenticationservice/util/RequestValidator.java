e88bb5359fbb9e769301d93f9f807c34a84a22c8
saadahmedscse <saadahmedscse@gmail.com>
2023-09-27 12:44:37 +0600

    validate admin and employee requests

--- AuthenticationService/src/main/java/com/saadahmedev/authenticationservice/util/RequestValidator.java
+++ AuthenticationService/src/main/java/com/saadahmedev/authenticationservice/util/RequestValidator.java
@@ -5,6 +5,7 @@ import com.saadahmedev.authenticationservice.dto.UserRequest;
 import com.saadahmedev.authenticationservice.entity.Role;
 import com.saadahmedev.authenticationservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -18,6 +19,9 @@ public class RequestValidator {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Value("${security.admin.secret-key}")
+    private String adminSecret;
 
     private final SimpleDateFormat dobFormatter = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
 
@@ -38,7 +42,12 @@ public class RequestValidator {
             return getError("Invalid date of birth format. Required dd-MMM-yyyy");
         }
         if (userRequest.getRole() == null) return getError("Role is required");
-        if (!userRequest.getRole().equals(Role.CUSTOMER)) return getError("Only CUSTOMER role will be accepted");
+
+        if (userRequest.getRole().equals(Role.ADMIN) || userRequest.getRole().equals(Role.EMPLOYEE)) {
+            if (userRequest.getAdminSecret() == null || userRequest.getAdminSecret().isEmpty()) return getError("Required admin secret key");
+            if (!userRequest.getAdminSecret().equals(adminSecret)) return getError("Invalid admin secret key");
+        }
+
         if (userRequest.getPassword() == null || userRequest.getPassword().isEmpty()) return getError("Password is required");
 
         return new ResponseEntity<>(HttpStatus.OK);
