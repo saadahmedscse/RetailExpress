ccfea80c42a3c8966ddd3c7f2e0c5934ad55d5de
saadahmedscse <saadahmedscse@gmail.com>
2023-09-27 12:26:24 +0600

    created createCustomer handler

---
+++ AuthenticationService/src/main/java/com/saadahmedev/authenticationservice/controller/AuthController.java
@@ -0,0 +1,20 @@
+package com.saadahmedev.authenticationservice.controller;
+
+import com.saadahmedev.authenticationservice.dto.UserRequest;
+import com.saadahmedev.authenticationservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/create/customer")
+    public ResponseEntity<?> createCustomer(@RequestBody UserRequest userRequest) {
+        return userService.createCustomer(userRequest);
+    }
+}
