8d64fff5cc38d268b61668b92e265070cff9d023
saadahmedscse <saadahmedscse@gmail.com>
2023-09-28 11:17:18 +0600

    added user login handler

--- AuthenticationService/src/main/java/com/saadahmedev/authenticationservice/controller/AuthController.java
+++ AuthenticationService/src/main/java/com/saadahmedev/authenticationservice/controller/AuthController.java
@@ -1,7 +1,9 @@
 package com.saadahmedev.authenticationservice.controller;
 
+import com.saadahmedev.authenticationservice.dto.LoginRequest;
 import com.saadahmedev.authenticationservice.dto.UserRequest;
 import com.saadahmedev.authenticationservice.service.UserService;
+import jakarta.annotation.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,5 +18,10 @@ public class AuthController {
     @PostMapping("/create")
     public ResponseEntity<?> createUser(@RequestBody UserRequest userRequest) {
         return userService.createUser(userRequest);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@Nullable @RequestBody LoginRequest loginRequest) {
+        return userService.login(loginRequest);
     }
 }
