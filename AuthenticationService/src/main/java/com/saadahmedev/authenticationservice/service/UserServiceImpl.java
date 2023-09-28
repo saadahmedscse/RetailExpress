fc16b12b04e27612881b816a161c6e2674f0ab60
saadahmedscse <saadahmedscse@gmail.com>
2023-09-28 11:16:28 +0600

    implemented login method

--- AuthenticationService/src/main/java/com/saadahmedev/authenticationservice/service/UserServiceImpl.java
+++ AuthenticationService/src/main/java/com/saadahmedev/authenticationservice/service/UserServiceImpl.java
@@ -1,19 +1,27 @@
 package com.saadahmedev.authenticationservice.service;
 
 import com.saadahmedev.authenticationservice.dto.ApiResponse;
+import com.saadahmedev.authenticationservice.dto.LoginRequest;
+import com.saadahmedev.authenticationservice.dto.LoginResponse;
 import com.saadahmedev.authenticationservice.dto.UserRequest;
 import com.saadahmedev.authenticationservice.entity.User;
 import com.saadahmedev.authenticationservice.repository.UserRepository;
+import com.saadahmedev.authenticationservice.security.JwtUtil;
 import com.saadahmedev.authenticationservice.util.RequestValidator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
 
     @Autowired
@@ -24,6 +32,12 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @Override
     public ResponseEntity<?> createUser(UserRequest userRequest) {
@@ -36,6 +50,19 @@ public class UserServiceImpl implements UserService {
         } catch (Exception e) {
             return ResponseEntity.internalServerError().body(new ApiResponse(false, e.getLocalizedMessage()));
         }
+    }
+
+    @Override
+    public ResponseEntity<?> login(LoginRequest loginRequest) {
+        ResponseEntity<?> validationResult = requestValidator.isLoginRequestValid(loginRequest);
+        if (validationResult.getStatusCode().isSameCodeAs(HttpStatus.BAD_REQUEST)) return validationResult;
+
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        log.info("User has been authenticated");
+        return new ResponseEntity<>(new LoginResponse(
+                true,
+                "Logged in successfully",
+                jwtUtil.generateToken(loginRequest.getUsername())), HttpStatus.OK);
     }
 
     private User mapUser(UserRequest userRequest) {
