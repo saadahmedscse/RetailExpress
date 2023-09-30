a8ff2332f0ea26a87bc9e1a495d8d37024eabdf7
saadahmedscse <saadahmedscse@gmail.com>
2023-09-30 23:33:27 +0600

    added some custom JPAql

--- AccountService/src/main/java/com/saadahmedev/accountservice/repository/AccountRepository.java
+++ AccountService/src/main/java/com/saadahmedev/accountservice/repository/AccountRepository.java
@@ -4,6 +4,18 @@ import com.saadahmedev.accountservice.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface AccountRepository extends JpaRepository<Account, Long> {
+public interface AccountRepository extends JpaRepository<Account, String> {
+    List<Account> findAllByUserId(long userId);
+
+    Optional<Account> findByAccountNumber(String accountNumber);
+
+    Optional<Account> findByUserIdAndAccountNumber(long userId, String accountNumber);
+
+    void deleteByUserIdAndAccountNumber(long userId, String accountNumber);
+
+//    void deleteAllByUserId(long userId);
 }
