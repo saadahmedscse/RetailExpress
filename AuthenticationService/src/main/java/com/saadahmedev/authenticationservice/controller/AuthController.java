6cc3be04df26b8dbefb2d6ecd5486a56bd4875e4
saadahmedscse <saadahmedscse@gmail.com>
2023-09-30 10:08:35 +0600

    added logout handler

--- AuthenticationService/src/main/java/com/saadahmedev/authenticationservice/controller/AuthController.java
+++ AuthenticationService/src/main/java/com/saadahmedev/authenticationservice/controller/AuthController.java
@@ -2,8 +2,10 @@ package com.saadahmedev.authenticationservice.controller;
 
 import com.saadahmedev.authenticationservice.dto.LoginRequest;
 import com.saadahmedev.authenticationservice.dto.UserRequest;
+import com.saadahmedev.authenticationservice.service.AuthService;
 import com.saadahmedev.authenticationservice.service.UserService;
 import jakarta.annotation.Nullable;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,9 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthService authService;
+
     @PostMapping("/create")
     public ResponseEntity<?> createUser(@RequestBody UserRequest userRequest) {
         return userService.createUser(userRequest);
@@ -22,6 +27,11 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@Nullable @RequestBody LoginRequest loginRequest) {
-        return userService.login(loginRequest);
+        return authService.login(loginRequest);
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<?> logoutUser(HttpServletRequest request) {
+        return authService.logout(request);
     }
 }
