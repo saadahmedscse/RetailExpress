19a89dd90c01b46cc491855c57a32a9d4359cdf0
saadahmedscse <saadahmedscse@gmail.com>
2023-10-04 19:31:26 +0600

    added onDepositEvent method

--- NotificationService/src/main/java/com/saadahmedev/notificationservice/service/NotificationService.java
+++ NotificationService/src/main/java/com/saadahmedev/notificationservice/service/NotificationService.java
@@ -3,5 +3,8 @@ package com.saadahmedev.notificationservice.service;
 public interface NotificationService {
 
     void onUserCreationEvent(String event);
+
+    void onDepositEvent(String event);
+
     void onAccountCreationEvent(String event);
 }
