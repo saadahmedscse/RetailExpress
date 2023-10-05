52b98f4fa455251216ca4e2bf3c1e1c2e10b3461
saadahmedscse <saadahmedscse@gmail.com>
2023-10-05 11:24:35 +0600

    created user controller

---
+++ UserService/src/main/java/com/saadahmedscse/userservice/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.saadahmedscse.userservice.controller;
+
+import com.saadahmedscse.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUser(@PathVariable long id) {
+        return userService.getUser(id);
+    }
+}
