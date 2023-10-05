c501442e0b41b9e7ec2fbf6a75a2cbefacfe6ce7
saadahmedscse <saadahmedscse@gmail.com>
2023-10-05 12:24:03 +0600

    send email on amount withdraw

--- NotificationService/src/main/java/com/saadahmedev/notificationservice/service/NotificationServiceImpl.java
+++ NotificationService/src/main/java/com/saadahmedev/notificationservice/service/NotificationServiceImpl.java
@@ -3,6 +3,7 @@ package com.saadahmedev.notificationservice.service;
 import com.google.gson.Gson;
 import com.saadahmedev.notificationservice.dto.KafkaAccountCreationEvent;
 import com.saadahmedev.notificationservice.dto.KafkaDepositEvent;
+import com.saadahmedev.notificationservice.dto.KafkaWithdrawEvent;
 import com.saadahmedev.notificationservice.dto.UserCreationEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +20,17 @@ public class NotificationServiceImpl implements NotificationService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    private static SimpleMailMessage email;
+
+    public NotificationServiceImpl() {
+        if (email == null) email = new SimpleMailMessage();
+    }
+
     @Override
     public void onUserCreationEvent(String event) {
         log.info(event);
         UserCreationEvent userCreationEvent = new Gson().fromJson(event, UserCreationEvent.class);
 
-        SimpleMailMessage email = new SimpleMailMessage();
         email.setTo(userCreationEvent.getEmail());
         email.setSubject(userCreationEvent.getSubject());
         email.setText(userCreationEvent.getMessage());
@@ -38,7 +44,6 @@ public class NotificationServiceImpl implements NotificationService {
         log.info(event);
         KafkaDepositEvent kafkaDepositEvent = new Gson().fromJson(event, KafkaDepositEvent.class);
 
-        SimpleMailMessage email = new SimpleMailMessage();
         email.setTo(kafkaDepositEvent.getEmail());
         email.setSubject(kafkaDepositEvent.getSubject());
         email.setSentDate(new Date(kafkaDepositEvent.getDepositTime()));
@@ -58,13 +63,31 @@ public class NotificationServiceImpl implements NotificationService {
         log.info(event);
         KafkaAccountCreationEvent kafkaAccountCreationEvent = new Gson().fromJson(event, KafkaAccountCreationEvent.class);
 
-        SimpleMailMessage email = new SimpleMailMessage();
         email.setTo(kafkaAccountCreationEvent.getEmail());
         email.setSubject(kafkaAccountCreationEvent.getSubject());
         email.setSentDate(new Date(kafkaAccountCreationEvent.getCreationTime()));
         String message = "Congratulations! A " + kafkaAccountCreationEvent.getAccountType() + " account has been opened.\n" +
                 "Your account number is: " + kafkaAccountCreationEvent.getAccountNumber() + "\n" +
                 "Account opening date: " + new Date(kafkaAccountCreationEvent.getCreationTime());
+        email.setText(message);
+
+        javaMailSender.send(email);
+    }
+
+    @Override
+    public void onWithdrawEvent(String event) {
+        log.info(event);
+        KafkaWithdrawEvent kafkaWithdrawEvent = new Gson().fromJson(event, KafkaWithdrawEvent.class);
+
+        email.setTo(kafkaWithdrawEvent.getEmail());
+        email.setSubject(kafkaWithdrawEvent.getSubject());
+        email.setSentDate(new Date(kafkaWithdrawEvent.getWithdrawTime()));
+        String message = kafkaWithdrawEvent.getWithdrawAmount() + " BDT has been withdraw from you account\n" +
+                "Account Number: " + kafkaWithdrawEvent.getAccountNumber() + "\n" +
+                "Account Type: " + kafkaWithdrawEvent.getAccountType() + "\n" +
+                "Previous Balance: " + kafkaWithdrawEvent.getPreviousAmount() + "\n" +
+                "Current Balance: " + kafkaWithdrawEvent.getCurrentAmount() + "\n" +
+                "Withdraw Time: " + new Date(kafkaWithdrawEvent.getWithdrawTime());
         email.setText(message);
 
         javaMailSender.send(email);
