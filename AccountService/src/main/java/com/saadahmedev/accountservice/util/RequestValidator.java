8837b7b44e60267d9e7f5ca28d0a1f95acd28a62
saadahmedscse <saadahmedscse@gmail.com>
2023-10-05 12:23:52 +0600

    validate withdraw request

--- AccountService/src/main/java/com/saadahmedev/accountservice/util/RequestValidator.java
+++ AccountService/src/main/java/com/saadahmedev/accountservice/util/RequestValidator.java
@@ -1,6 +1,7 @@
 package com.saadahmedev.accountservice.util;
 
 import com.saadahmedev.accountservice.dto.DepositRequest;
+import com.saadahmedev.accountservice.dto.WithdrawRequest;
 import com.saadahmedev.accountservice.entity.Account;
 import com.saadahmedev.accountservice.entity.AccountType;
 import com.saadahmedev.accountservice.service.ServerResponse;
@@ -18,6 +19,16 @@ public class RequestValidator {
         if (depositRequest.getAmount() < 100) return ServerResponse.badRequest("Minimum deposit amount is 100 BDT");
         if (depositRequest.getAccountType() == null) return ServerResponse.badRequest("Account type is required. Eg.. SAVINGS, CURRENT, FDR, RDR, JOINT");
         if (!accountMap.containsKey(depositRequest.getAccountType())) return ServerResponse.badRequest("Customer has not opened any " + depositRequest.getAccountType() + " yet");
+
+        return ServerResponse.ok();
+    }
+
+    public static ResponseEntity<?> isWithdrawRequestValid(WithdrawRequest withdrawRequest, Map<AccountType, Account> accountMap) {
+        if (withdrawRequest == null) return ServerResponse.badRequest("Withdraw request body cannot be null");
+        if (withdrawRequest.getAmount() == null) return ServerResponse.badRequest("Withdraw amount is required");
+        if (withdrawRequest.getAmount() < 50) return ServerResponse.badRequest("Minimum withdraw amount is 50 BDT");
+        if (withdrawRequest.getAccountType() == null) return ServerResponse.badRequest("Account type is required. Eg.. SAVINGS, CURRENT, FDR, RDR, JOINT");
+        if (!accountMap.containsKey(withdrawRequest.getAccountType())) return ServerResponse.badRequest("Customer has not opened any " + withdrawRequest.getAccountType() + " yet");
 
         return ServerResponse.ok();
     }
