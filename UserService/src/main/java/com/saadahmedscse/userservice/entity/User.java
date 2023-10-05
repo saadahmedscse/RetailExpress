91bff6140dc447a6ae55020ecbfd15fd928cac4c
saadahmedscse <saadahmedscse@gmail.com>
2023-10-05 11:24:41 +0600

    created user entity

---
+++ UserService/src/main/java/com/saadahmedscse/userservice/entity/User.java
@@ -0,0 +1,33 @@
+package com.saadahmedscse.userservice.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    @JsonIgnore
+    private String password;
+    private String email;
+    private String phone;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    private Date createdAt;
+    private Date updatedAt;
+}
